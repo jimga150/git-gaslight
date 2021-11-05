e2c4cf7dd04ddb0eaeb06348f2cfc1a92e38d2da
jimga150 <jimbosned@gmail.com>
2021-11-05 16:53:55 -0400

    Fixed install script

--- install_git_gaslight.sh
+++ install_git_gaslight.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 BUILD_DIR=$(pwd)/../git_build
+ALIAS_CMD=$(printf "alias git='%s/bin/git'\n" $BUILD_DIR)
 
 mkdir $BUILD_DIR
-make configure && ./configure --prefix=$BUILD_DIR && make all && make install
-ALIAS_CMD=$(printf "alias git='%s/bin/git'\n" $BUILD_DIR)
-echo $ALIAS_CMD >> ~/.bash_aliases
-source ~/.bashrc || $ALIAS_CMD 
+make configure && ./configure --prefix=$BUILD_DIR && make all && make install && echo $ALIAS_CMD >> ~/.bash_aliases
